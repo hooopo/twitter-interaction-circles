ac6dbc6782a7a1072c9ca5ecc6705318e353ac84
Hooopo Wang <hoooopo@gmail.com>
2023-05-24 16:20:53 +0800

    test

--- src/index.js
+++ src/index.js
@@ -14,38 +14,29 @@ dotenv.config();
  * This is the main function of the app. It need to be a function because we can't have a top level await.
  */
 async function main() {
-	// Create an instance of the API client using the consumer keys for your app
-	const client = new Twitter({
-		consumer_key: process.env.CONSUMER_KEY,
-		consumer_secret: process.env.CONSUMER_SECRET,
-	});
 
-	// Use the previous client to fetch the bearer token
-	// This method gives you an application-only token specifically for read-only access to public information.
-	const bearer = await client.getBearerToken();
-
-	// Create a new twitter client with this token.
-	// We assign this client to a global variable so we can access it in the api module
-	globalThis.TwitterClient = new Twitter({
-		bearer_token: bearer.access_token,
-	});
 
 	// fetch the information of the logged in user
 	// instead of getMe you could replace it with another method to get a third user to generate their circles
-	const user = await getUser("WHATEVER_USERNAME_YOU_WANT");
+	const user = {screen_name: "realDonaldTrump", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"};
 
 	// this is how many users we will have for each layer from the inside out
 	const layers = [8, 15, 26];
 
-	// fetch the interactions
-	const data = await getInteractions(user.screen_name.toLowerCase(), layers);
-
 	// render the image
 	await render([
 		{distance: 0, count: 1, radius: 110, users: [user]},
-		{distance: 200, count: layers[0], radius: 64, users: data[0]},
-		{distance: 330, count: layers[1], radius: 58, users: data[1]},
-		{distance: 450, count: layers[2], radius: 50, users: data[2]},
+		{distance: 200, count: layers[0], radius: 64, 
+			users: [
+				{screen_name: "joebiden", avatar: "https://pbs.twimg.com/profile_images/1276559966212654081/0oGAop6Q_400x400.jpg"},
+				{screen_name: "kanyewest", avatar: "https://pbs.twimg.com/profile_images/1227379798656221696/_d7a9xlg_400x400.jpg"},
+				{screen_name: "elonmusk", avatar: "https://pbs.twimg.com/profile_images/1295975423654977537/dHw9JcrK_400x400.jpg"},
+				{screen_name: "BarackObama", avatar: "https://pbs.twimg.com/profile_images/822547732376207360/5g0FC8XX_400x400.jpg"},
+				{screen_name: "katyperry", avatar: "https://pbs.twimg.com/profile_images/1195380678477143553/USVh7v6B_400x400.jpg"},
+				{screen_name: "rihanna", avatar: "https://pbs.twimg.com/profile_images/1279342408751986177/oOeFyFz_400x400.jpg"}
+			]
+
+			},
 	]);
 
 	// Look at the arguments passed to the cli. If one of them is --text then we want to render a text version of the image too
