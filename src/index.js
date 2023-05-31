4bf7f51eb54384ec3d079a56f7fb35eaa21720fa
Hooopo Wang <hoooopo@gmail.com>
2023-05-31 23:05:49 +0800

    add more

--- src/index.js
+++ src/index.js
@@ -28,11 +28,15 @@ function conn() {
       rejectUnauthorized: true
     }
   });
+
+	const results = [];
 	
-	conn.query("SELECT * FROM users limit 10", function (err, result, fields) {
+	conn.query("SELECT avatar_url as avatar, login as screen_name  FROM users limit 49", function (err, result, fields) {
 		if (err) throw err;
-		console.log(result);
+		results.push(result);
 	});
+
+	return results;
 
 }
 
@@ -40,6 +44,21 @@ function conn() {
  * Load the environment variables from the .env file
  */
 dotenv.config();
+
+function splitArray(input, groupSizes) {
+  let output = [];
+  for (let size of groupSizes) {
+    let group = input.splice(0, size);
+    output.push(group);
+  }
+
+  // If any items are left, add them to the last group
+  if (input.length > 0) {
+    output[output.length - 1] = output[output.length - 1].concat(input);
+  }
+
+  return output;
+}
 
 /**
  * This is the main function of the app. It need to be a function because we can't have a top level await.
@@ -54,23 +73,17 @@ async function main() {
 	// this is how many users we will have for each layer from the inside out
 	const layers = [8, 15, 26];
 
-	conn();
+	results = conn();
+
+	const users = splitArray(results, layers);
 
 
 	// render the image
 	await render([
 		{distance: 0, count: 1, radius: 110, users: [user]},
-		{distance: 200, count: layers[0], radius: 64, 
-			users: [
-				{screen_name: "joebiden", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"},
-				{screen_name: "kanyewest", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"},
-				{screen_name: "elonmusk", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"},
-				{screen_name: "BarackObama", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"},
-				{screen_name: "katyperry", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"},
-				{screen_name: "rihanna", avatar: "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg"}
-			]
-
-			},
+		{distance: 200, count: layers[0], radius: 64, users: users[0]},
+		{distance: 330, count: layers[1], radius: 58, users: data[1]},
+		{distance: 450, count: layers[2], radius: 50, users: data[2]},
 	]);
 
 	// Look at the arguments passed to the cli. If one of them is --text then we want to render a text version of the image too
